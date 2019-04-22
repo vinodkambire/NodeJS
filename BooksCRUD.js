95a2fd331d71b86d545a694be1581164735d7a1b
Vinod Kambire <vinod@techilaservices.com>
2019-04-22 12:27:37 +0530

    Create BooksCRUD.js
    books curd without database

---
+++ BooksCRUD.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var app = express();
+app.use(bodyParser.json());
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'TestNode'
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('connected');
+});
+
+connection.query('SELECT * FROM Books', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The result is: ', results);
+});
+   
+connection.end();
+
+var server = app.listen(8080, () => {
+    console.log('Server started');
+});
+
+var books = [
+    {
+        name: 'A.txt',
+        author: 'A'
+    },
+    {
+        name: 'B.txt',
+        author: 'B'
+    },
+    {
+        name: 'C.txt',
+        author: 'C'
+    },
+    {
+        name: 'D.txt',
+        author: 'D'
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello Books!!');
+});
+
+app.get('/books', (req, res) => {
+    res.status(200).json(books);
+});
+
+app.get('/books/:bookName', (req, res) => {
+    var fetchedBook = [];
+    const bookName = req.params['bookName'];
+
+    for (let index = 0; index < books.length; index++) {
+        if(books[index].name === bookName) {
+            fetchedBook.push(books[index]);
+        }
+    }
+    res.status(200).json(fetchedBook);
+});
+
+app.post('/addbooks', (req, res) => {
+    books.push(req.body);
+    res.send(books);
+    //res.status(200).json(books);
+});
+
+app.delete('/removebooks/:name', (req, res) => {
+    
+    const bookName = req.params['name'];
+    for (let index = 0; index < books.length; index++) {
+        if(books[index].name === bookName) {
+            books.splice(index, 1);
+        }
+    }
+    res.status(200).json(books);
+});
+
+app.get('/*', (req, res) => {
+    res.redirect('/books');
+});
